9926b684046e832c6d3de9dbd3bcf749dade07d8
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2023-07-13 23:11:42 +0530

    Initial COmmit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 import express from 'express'
+import dotenv from 'dotenv'
 
 const app = express();
+dotenv.config()
+
+app.get('/', (req,res) => {
+  res.send("Working")
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`server is running on ${process.env.PORT}`)
