c80cdc4024e83f63f815a8bba7b743f4bc82dc20
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2023-07-25 23:48:22 +0530

    gitignore

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
+
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  // Load the appropriate .env file based on the build mode
+  const env = dotenv.config({
+    path: `.env.${mode}`,
+  }).parsed;
+
+  // Process the environment variables and make them available to the client-side code
+  const processEnv = {};
+  for (const key in env) {
+    processEnv[`import.meta.env.${key}`] = env[key];
+  }
+
+  return {
+    plugins: [react()],
+    define: processEnv,
+  };
+});
